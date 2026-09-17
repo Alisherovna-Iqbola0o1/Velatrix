c2924ef9ed7876c7cda933081f29a6c604814bcc
Iqbola <Alisherovna-Iqbola0o1@gmail.com>
2026-09-17 09:42:31 +0500

    Add CORS and JWT configurations

--- courses/urls.py
+++ courses/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", CourseListView.as_view(), name="course-list"),
     path("<slug:slug>/", CourseDetailView.as_view(), name="course-detail"),
     path("<uuid:course_id>/sections/", SectionListView.as_view(), name="section-list"),
+    path('lessons/<uuid:lesson_id>/questions/', LessonQuestionsView.as_view(), name='lesson-questions'),
 ]
